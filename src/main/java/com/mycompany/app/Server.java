60313eab7f5fc9e258a85817f8473d6619123d32
vijejain <67664521+vijejain@users.noreply.github.com>
2021-04-13 11:55:18 -0700

    Create Server.java

---
+++ src/main/java/com/mycompany/app/Server.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class Server
+{
+    public void bar()
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    private void foo(String p) {
+       String s = "aloha";
+       System.out.println(String.format("This is a faulty message: %i", s));
+    }
+
+}
