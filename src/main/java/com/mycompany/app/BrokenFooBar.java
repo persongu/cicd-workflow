3db53c031627a02868d223de6f0b68b49ed1b259
vijejain <67664521+vijejain@users.noreply.github.com>
2021-04-14 14:54:07 -0700

    Create BrokenFooBar.java

---
+++ src/main/java/com/mycompany/app/BrokenFooBar.java
@@ -0,0 +1,27 @@
+package com.mycompany.app;
+import javax.crypto.Cipher;
+import java.lang.Exception;
+/**
+ * Hello world!
+ *
+ */
+public class BrokenFooBar
+{
+    public void bar()
+    {
+        System.out.println( "Hello World!" );
+    }
+    private void foo(String p) {
+       String s = "aloha";
+       System.out.println(String.format("This is a faulty message: %i", s));
+       System.out.format("No %s",1);
+       if ("1" == "2") {
+        // do nothing
+       }
+       cipher(s);
+    }
+    private void cipher(String s) throws Exception
+    {
+        Cipher c = Cipher.getInstance(s);
+    }
+}
